54f596a29b16945b922abff683297d9d9d4b7d94
Loic RENOUD-GRAPPIN <lrenoud-@ubuntu>
2016-04-11 17:09:01 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,19 @@
+#! /usr/bin/env bash
+
 # construction image -t pour le nom et . pour le chemin ou ce trouve le dockerfile
-docker build -t bensisko/wouf-educ .
+if ! docker inspect bensisko/wouf-educ &> /dev/null; then
+    docker build -t bensisko/wouf-educ .
+else
+    echo "Image is already built"
+fi
+
 #  cree le conteneur qui lance le serveur
-# --rm pour le supprimer a la fin de l'execution
 # -it recuperer la sortie standard 
 # -v $PWD:/app partage du dossier courant avec le conteneur
 # -p exposition du port 9999 du conteneur vers le port 80 du host 
-docker run --rm -it -v $PWD:/app -p 80:9999 bensisko/wouf-educ
+# -d lance le conteneur en arrière plan (detached)
+if ! docker inspect wouf-educ &> /dev/null; then
+    docker run --name wouf-educ -d -it -v $PWD:/app -p 80:9999 bensisko/wouf-educ
+else
+    echo "Server is already running"
+fi
