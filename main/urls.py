2723ea86108d37830b46c66cbbea54e29055a829
loic renoud_grappin <loic renoud_grappin>
2016-04-13 03:54:18 +0200

    modif wiews temoignage and contact

--- main/urls.py
+++ main/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     url(r'^reeducation', views.reeducation, name='reeducation'),
     url(r'^promenades', views.promenades, name='promenades'),
     url(r'^mention', views.mention, name='mention')
+    url(r'^temoignageOk', views.temoignageOk, name='temoignageOk')
 ,]
