bf168f9cb85cdce62486c7717d3e8a78a6c7cedf
Loic RENOUD-GRAPPIN <lrenoud-@ubuntu>
2016-04-19 20:04:57 +0200

    mail

--- woufeduc/settings.py
+++ woufeduc/settings.py
@@ -140,7 +140,6 @@ MARKDOWN_EDITOR_SKIN = 'simple'
 FILE_UPLOAD_MAX_MEMORY_SIZE = 5000000
 
 #email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'loicrg69@gmail.com'
 EMAIL_PORT = 587
